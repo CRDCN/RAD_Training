6325343d2efcd394499c0d44a61a9e3a0ba18039
Thierry Letendre <thierry.letendre@mail.utoronto.ca>
2025-04-24 11:25:58 -0400

    Started python code

--- Python Adaptation/Python_Metadata.py
+++ Python Adaptation/Python_Metadata.py
@@ -1,1 +1,14 @@
 # PYTHON METADATA ADAPTATION FILE
+# Using Anaconda - Python 3.9.13. 
+
+import os
+import pandas as pd
+from bs4 import BeautifulSoup
+import xml.dom.minidom
+from xml.etree import ElementTree
+import lxml.etree as etree
+
+
+
+path = "/Users/thierryletendre/Desktop/CRDCN/RAD_Training/Resources/Data/2025_03_27_WPDATA.csv"
+#path = '' # Insert path here
